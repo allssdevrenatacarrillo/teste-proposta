a0ebba4259fc768949f5e70eea77ac83cfa9de84
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2022-02-21 11:18:28 -0300

    teste 21-02 08 watermark

--- reports_with_watermark/models/res_company.py
+++ reports_with_watermark/models/res_company.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# from odoo import fields, models, api
+
 from odoo import fields, models, api
+from jinja2 import Template
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ResCompany(models.Model):
@@ -46,3 +51,29 @@ class ResCompany(models.Model):
             self.rotate_angle = ""
             self.font_size = ""
             self.font_color = ""
+
+class SaleOrderTmlCustom(models.Model):
+    _inherit = 'sale.order.template'
+
+    logo = fields.Binary('Logo')
+    body_html = fields.Html('Conteúdo da Proposta')
+    contract_html = fields.Html('Conteúdo do Contrato')
+
+
+class SaleOrderCustom(models.Model):
+    _inherit = 'sale.order'
+
+    parent_order = fields.Many2one('sale.order', "Proposta Relacionada")
+
+    def render_html(self, body_html):
+        if not body_html:
+            return ""
+        body_html = body_html.replace("$PageBreak", "".join(["<br/>" for i in range(1, 11)]))
+        tm = Template(body_html)
+        msg = tm.render(object=self)
+
+        _logger.warning(f'################# FIELD: {body_html} #################')
+        _logger.warning(f'################# TEMPLATE: {tm} #################')
+        _logger.warning(f'################# MESSAGE: {msg} #################')
+
+        return msg
