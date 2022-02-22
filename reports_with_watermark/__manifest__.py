960530eea86104e4d8f279dc4cab58d6332cc78e
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2022-02-22 11:43:25 -0300

    teste 22-02 watermark 19

--- reports_with_watermark/__manifest__.py
+++ reports_with_watermark/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of AktivSoftware See LICENSE file for full copyright
 # and licensing details.
 {
-    'name': "Reports with Watermark 1.0",
+    'name': "Proposta Dinâmica ALLSS - Watermark 1.0",
     'summary': """
             Generates reports with watermark as per the image uploaded.
              """,
